a14217d9dffca4c0057e1bf83d93c0f2a58a4fee
unknown <ranjan.muku@gmail.com>
2022-12-11 08:34:21 +0530

    Day 1 With Redwood

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,38 @@
+import type { FindPosts } from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ posts }: CellSuccessProps<FindPosts>) => {
+  return (
+    <>
+      {posts.map((article) => (
+        <article key={article.id}>
+          <header>
+            <h2>{article.title}</h2>
+          </header>
+          <p>{article.body}</p>
+          <div>Posted at: {article.createdAt}</div>
+        </article>
+      ))}
+    </>
+  )
+}
---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>This site is created for demonstrate purpose</p>
+    </>
+  )
+}
+
+export default AboutPage
